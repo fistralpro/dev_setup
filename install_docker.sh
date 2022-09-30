d0e79a6832d4bb1c720104e74ddff6ec3a06f40d
fistralpro <80547781+fistralpro@users.noreply.github.com>
2022-09-30 22:09:33 +0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -8,9 +8,7 @@ sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-relea
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
 
 # 3. Use stable repository for Docker 
-echo \ 
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \ 
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
 
 # 4. Install Docker 
 sudo apt-get update 
