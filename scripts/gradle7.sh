5f25eb058c00e8efa59c9a3f00b90e51e24e840c
fistralpro <fistralpro@outlook.com>
2022-12-31 20:22:44 +0000

    gradle 7 initial installation script

---
+++ scripts/gradle7.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Ubuntu 20.04 amd64 install of gradle
+# instructions from: https://gradle.org/install/
+# run with
+#    sudo bash gradle7.sh
+[[ "$EUID" -ne 0 ]] && echo "This script must be run as root" && exit 1
+
+VERSION=7.6
+
+wget https://services.gradle.org/distributions/gradle-${VERSION}-bin.zip -P /tmp
+unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip
+
+ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
+echo "export GRADLE_HOME=/opt/gradle/latest" > /etc/profile.d/gradle.sh
+echo "export PATH=${GRADLE_HOME}/bin:${PATH}" >> /etc/profile.d/gradle.sh
+chmod +x /etc/profile.d/gradle.sh
+source /etc/profile.d/gradle.sh
+
+gradle --v
