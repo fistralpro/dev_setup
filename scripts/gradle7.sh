c55ec13cb22439f3d756383c701c324e8ce6c27b
fistralpro <80547781+fistralpro@users.noreply.github.com>
2023-01-15 16:08:05 +0000

    Update gradle7.sh
    Attempt to auto source

--- scripts/gradle7.sh
+++ scripts/gradle7.sh
@@ -3,6 +3,7 @@
 # instructions from: https://gradle.org/install/
 # run with
 #    sudo bash gradle7.sh
+[[ "$EUID" -ne 0 ]] && echo "This script must be run as root e.g: sudo bash gradle7.sh" && exit 1
 
 VERSION=7.6
 
@@ -11,8 +12,8 @@ unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip
 
 ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
 echo "export GRADLE_HOME=/opt/gradle/latest" > /etc/profile.d/gradle.sh
-echo "export PATH=${GRADLE_HOME}/bin:${PATH}" >> /etc/profile.d/gradle.sh
+echo "export PATH=/opt/gradle/latest/bin:${PATH}" >> /etc/profile.d/gradle.sh
 chmod +x /etc/profile.d/gradle.sh
-source /etc/profile.d/gradle.sh
 
+source /etc/profile.d/gradle.sh
 gradle --v
