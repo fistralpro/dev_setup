0757dade975b2f7180b71f166b13c09bec5f6bc4
fistralpro <fistralpro@outlook.com>
2023-04-10 14:12:51 +0100

    Add python3

---
+++ scripts/python3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Ubuntu 20.04 amd64 install of nodejs
+# run with
+#    sudo bash node.sh
+[[ "$EUID" -ne 0 ]] && echo "This script must be run as root e.g: sudo bash python3.sh" && exit 1
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y python3-pip
+sudo apt install -y  build-essential libssl-dev libffi-dev python3-dev python3-venv
